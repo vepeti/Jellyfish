5aa56d1b18677077b0cec293c988636e4c605cad
vepeti <venyercsan.peter@gmail.com>
2018-04-19 16:08:21 +0200

    Update template.php

--- template.php
+++ template.php
@@ -17,7 +17,7 @@ public function set($key, $value)
 
 public function output()
 {
-if (!file_exists($this->file))
+if (!file_exists($this->file)) 
 {
     return "Error loading template file ($this->file).";
 }
@@ -39,17 +39,16 @@ private function condition($string)
 {
 $string=preg_replace_callback("/\{\{ ?if (\(.+\)) ?}}(.+)\{\{ ?endif ?}}/sU", function ($found)
 {
-$condstring=preg_replace_callback("/\[@(.+)]/", function ($condvars)
-{return $this->values[$condvars[1]];}, $found[1]);
-
+//    $condstring=$this->main_subst($found[1]);
+    $condstring=$this->filters($found[1]);
     $cond=eval("return $condstring;");
     if ($cond)
     {
-        return $found[2];
+	return $found[2];
     }
     else
     {
-        return NULL;
+	return NULL;
     }
 }, $string);
 return $string;
@@ -62,19 +61,19 @@ $string=preg_replace_callback("/\{\{ ?for ((\w+)=>)?(\w+) in \[@(.+)] ?}}(.+)\{\
     $row=NULL;
     foreach($this->values[$found[4]] as $key=>$element)
     {
-        if (preg_match("/\[@".$found[3]."](\[.+])/", $found[5]))
-        {
-            $index=preg_replace_callback("/^.*\[@".$found[3]."](\[.+]).*$/sU", function($dim){return $dim[1];}, $found[5]);
-            $str='return $element'.$index.';';
-            $var=eval($str);
-            $row.=preg_replace("/\[@".$found[3]."](\[.+])/U", $var, $found[5]);
-        }
-        else
-        {
-            $string=preg_replace("/\[@".$found[3]."]/", $element, $found[5]);
-            $string=preg_replace("/\[@".$found[2]."]/", $key, $string);
-            $row.=$string;
-        }
+	if (preg_match("/\[@".$found[3]."](\[.+])/", $found[5]))
+	{
+	    $index=preg_replace_callback("/^.*\[@".$found[3]."](\[.+]).*$/sU", function($dim){return $dim[1];}, $found[5]);
+	    $str='return $element'.$index.';';
+	    $var=eval($str);
+	    $row.=preg_replace("/\[@".$found[3]."](\[.+])/U", $var, $found[5]);
+	}
+	else
+	{
+	    $string=preg_replace("/\[@".$found[3]."]/", $element, $found[5]);
+	    $string=preg_replace("/\[@".$found[2]."]/", $key, $string);
+	    $row.=$string;
+	}
     }
 $row=$this->condition($row);
 return $row;
@@ -89,61 +88,54 @@ $string=preg_replace_callback("/\{\{ ?(.+?) ?\| ?default\((.*)\) ?}}/U", functio
 {
     if (isset($this->values[$found[1]]))
     {
-        return $this->values[$found[1]];
+	return $this->values[$found[1]];
     }
     else
     {
-        return $found[2];
+	return $found[2];
     }
 }, $string);
 
 // ARRAY FUNCTIONS
 $string=preg_replace_callback("/\{\{ ?\[@(.+?)] ?\| ?(
-|count|rand|first|last|min|join\((.*)\)|
+|count|rand|first|last|min|max|join\((.*)\)|
 ) ?}}/U", function($found)
 {
     if (is_array($this->values[$found[1]]))
     {
-        switch ($found[2])
-        {
-            case "count":
-                return count($this->values[$found[1]]);
-                break;
-            case "rand":
-                return array_rand($this->values[$found[1]]);
-                break;
-            case "first":
-                return reset($this->values[$found[1]]);
-                break;
-            case "last":
-                return end($this->values[$found[1]]);
-                break;
-            case "min":
-                return min($this->values[$found[1]]);
-                break;
-            case "max":
-                return end($this->values[$found[1]]);
-                break;
-            case "join($found[3])":
-                return implode($found[3], $this->values[$found[1]]);
-                break;
-        }
+	switch ($found[2])
+	{
+	    case "count":
+		return count($this->values[$found[1]]);
+		break;
+	    case "rand":
+		return array_rand($this->values[$found[1]]);
+		break;
+	    case "first":
+		return reset($this->values[$found[1]]);
+		break;
+	    case "last":
+		return end($this->values[$found[1]]);
+		break;
+	    case "min":
+		return min($this->values[$found[1]]);
+		break;
+	    case "max":
+		return end($this->values[$found[1]]);
+		break;
+	    case "join($found[3])":
+		return implode($found[3], $this->values[$found[1]]);
+		break;
+	}
     }
     else
     {
-        return "Not an Array";
+	return "Not an Array";
     }
 }, $string);
 
 // VARIABLE AND ARRAY SUBSTITUTIONS
-$string=preg_replace_callback("/\[@(.+)]((\[.+]){2,})/", function($found)
-{return eval('return $this->values[$found[1]]'."$found[2];");}, $string);
-
-$string=preg_replace_callback("/\[@(.+)]\[(.+)]/U", function($found)
-{return $this->values[$found[1]][$found[2]];}, $string);
-
-$string=preg_replace_callback("/\[@(.+)]/U", function($found)
-{return $this->values[$found[1]];}, $string);
+$string=$this->main_subst($string);
 
 // STRING FUNCTIONS
 if (preg_match("/\{\{.+?}}/", $string))
@@ -172,6 +164,19 @@ if (preg_match("/\{\{.+?}}/", $string))
     ) ?}}/", function($found)
     {return @eval("return hash($found[2],$found[1]);") ? @eval("return hash($found[2],$found[1]);") : "$found[1] is Not valid Number";}, $string);
 }
+return $string;
+}
+
+private function main_subst($string)
+{
+$string=preg_replace_callback("/\[@(.+)]((\[.+]){2,})/", function($found)
+{return eval('return $this->values[$found[1]]'."$found[2];");}, $string);
+
+$string=preg_replace_callback("/\[@(.+)]\[(.+)]/U", function($found)
+{return $this->values[$found[1]][$found[2]];}, $string);
+    
+$string=preg_replace_callback("/\[@(.+)]/U", function($found)
+{return $this->values[$found[1]];}, $string);
 return $string;
 }
 
